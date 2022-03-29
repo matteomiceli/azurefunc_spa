3e98c251ede3717f5258eb438f538c8f28922220
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-29 12:22:00 -0700

    vue client displaying content

---
+++ vue-client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Occupation</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in data" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.firstName + " " + item.lastName }}</td>
+                <td>{{ item.occupation }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data: () => {
+        return {
+            data: [],
+            url: "/api/toons",
+        };
+    },
+
+    async mounted() {
+        const res = await fetch(this.url);
+        this.data = await res.json();
+    },
+};
+</script>
+
+<style></style>
